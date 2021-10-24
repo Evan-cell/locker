14027bd68d03654c3688cf3631df780dee4b1e56
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-10-24 23:25:36 +0300

    created class test user

--- user-test.py
+++ user-test.py
@@ -1,2 +1,10 @@
 import unittest
 from user import user
+
+class Testuser(unittest.TestCase):
+    
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
